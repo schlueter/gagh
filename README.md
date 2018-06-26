36d8d0e9338aab05f2ac640de0cb09321b319f00
Schlueter <bs@schlueter.blue>
2018-06-25 20:10:46 -0400

    Appropriately consume label and unlabeled events

--- README.md
+++ README.md
@@ -7,14 +7,18 @@ This project aims to make consuming and taking action on these hooks as easy as 
 Initially this project is being built as part of Refinery29's 2018 summer hackathon, and so the scope must necessarily be limited. This is the list of my goals in general priority order to accomplish.
 
 -  [x] Expose a server which listens for and consumes a simple json blob *(Done prior to hackathon)*
--  [ ] Consume the pull request labeled event
--  [ ] Consume the pull request unlabeled event
+-  [x] Consume the pull request labeled event
+-  [x] Consume the pull request unlabeled event
 -  [ ] Trigger http call on event
 -  [ ] On a 'test' labeled event, run tests
 -  [ ] Run the tests in a container
 -  [ ] Make the event to trigger tests configurable
 -  [ ] Run the tests from a config file in the repo
 
+## Features
+- A server is exposed on a port­currently only configurable in the code.
+- Labeled and unlabled events are acknowledged. These are not yet tested against real GitHub events.
 
 
+### Server to consume json blob
 
--- github_hook_handler.go
+++ github_hook_handler.go
@@ -6,12 +6,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+const port = ":9999"
 
 func main() {
 	router := http.NewServeMux()
 	router.Handle("/", githubHookHandler())
-	err := http.ListenAndServe(":9999", githubHookHandler())
+	err := http.ListenAndServe(port, githubHookHandler())
 	log.Fatal(err)
 }
 
@@ -30,45 +33,52 @@ func githubHookHandler() http.Handler {
 	})
 }
 
-type IssuesEvent struct {
-	Action       string
-	Number       int
-	Repository   Repository
-	Pull_request PullRequest
+type GitHubEvent struct {
+	Repository Repository `json:"repository"`
+	// Sender Sender `json:"sender"`
 }
 
 type Repository struct {
-	Name      string
-	Full_name string
+	Name      string `json:"name"`
+	Full_name string `json:"full_name"`
+}
+
+type PullRequestEvent struct {
+	GitHubEvent
+	Action       string      `json:"action"`
+	Number       int         `json:"number"`
+	Pull_request PullRequest `json:"pull_request"`
+	Label        string      `json:"label"`
 }
 
 type PullRequest struct {
-	Html_url string
-	Head     GitRef
+	Html_url string `json:"html_url"`
+	Head     GitRef `json:"head"`
 }
 
 type GitRef struct {
-	Label string
+	Label string `json:"label"`
 }
 
 func handle_webhook(body []byte) {
-	var issues_event IssuesEvent
-	err := json.Unmarshal(body, &issues_event)
+	var pull_request_event PullRequestEvent
+	err := json.Unmarshal(body, &pull_request_event)
 	if err != nil {
 		log.Print("error:", err)
 		return
 	}
-	if issues_event.Action == "" {
+	if pull_request_event.Action == "" {
 		log.Print("Noop: Recieved event without an action to act on.")
 		return
 	}
-	log.Print("Action: Handling", issues_event.Action)
-	switch action := issues_event.Action; action {
+	log.Print("Action: Handling %s", pull_request_event.Action)
+	var pull_request_number = strconv.Itoa(pull_request_event.Number)
+	switch action := pull_request_event.Action; action {
 	case "labeled":
-		fmt.Printf("labeled\n")
+		fmt.Printf("Labeled PR #%s %s in %s\n", pull_request_number, pull_request_event.Label, pull_request_event.Repository.Name)
 	case "unlabeled":
-		fmt.Printf("unlabeled\n")
+		fmt.Printf("Unlabeled PR #%s %s in %s\n", pull_request_number, pull_request_event.Label, pull_request_event.Repository.Name)
 	default:
-		fmt.Printf("%s\n", action)
+		fmt.Printf("Unknown action %s\n", action)
 	}
 }
